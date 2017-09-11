a0e80c0b8b0e215fd2a1bd4ccb1bb4d851b00172
Krister Riska <31847374+kristerriska@users.noreply.github.com>
2017-09-11 14:42:34 +0300

    Lisatud esimene php osa

--- esimene.php
+++ esimene.php
@@ -7,5 +7,12 @@
 <body>
 	<h1>Krister Riska</h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+	<?php
+		echo "<p>Kõige esimene php abil väljastatud sõnum.</p>";
+		echo "<p>Täna on ";
+		echo date("d.m.Y");
+		echo ".</p>";
+		echo "<p>Lehe avamise hetkel oli kell " .date("H:i:s") .".</p>";
+	?>
 </body>
 </html>
