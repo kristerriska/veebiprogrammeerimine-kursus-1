2e0328cbd0516e30b9b3ca503f8c80229ad194c3
Krister Riska <31847374+kristerriska@users.noreply.github.com>
2017-09-11 13:49:00 +0300

    Esimene php fail, kus on vaid HTML

---
+++ esimene.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Krister Riska veebiprogrammeerimine</title>
+	</head>
+<body>
+	<h1>Krister Riska</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+</body>
+</html>
