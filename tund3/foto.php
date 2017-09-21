739f23152880d1e1541986d8821d4b28b6b9b5ea
Krister Riska <kristerriska@users.noreply.github.com>
2017-09-21 19:43:42 +0300

    Tund 3 materjalid, koos pildiga Ülikoolist

---
+++ tund3/foto.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Krister Riska veebiprogrammeerimine
+	</title>
+</head>
+<body>
+	
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+	<h2>Pilt ülikoolist</h2>
+	<img src="././pics/tlu.jpg" alt="Tallinna ülikool">
+	
+</body>
+</html>
